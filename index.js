d638dd8e60725b51e0092eac7d891ca67ae2fed9
SanjanaBasuthkar <basuthkarsanjana0@gmail.com>
2024-12-21 01:26:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-export const SHIPPING=5.55
-export const TAXES=5
-export const ITEMS_PER_PAGE=10
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {Provider} from 'react-redux'
+import { store } from './app/store';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme/theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <Provider store={store}>
+              <App />
+              <ToastContainer position='top-right' autoClose={1500} closeOnClick/>
+        </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
